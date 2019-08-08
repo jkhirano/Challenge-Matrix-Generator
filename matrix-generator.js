93c821ed841284bf4258774ccd7ae2581f935c59
Janelle K Hirano <jaahnelle@gmail.com>
2019-08-07 18:31:41 -1000

    finished

--- matrix-generator.js
+++ matrix-generator.js
@@ -1,5 +1,22 @@
+"use strict";
+
 function matrixGenerator(rows, columns) {
   // do work here
+
+  let final = [];
+
+  for (x = 0; x < rows; x++) {
+    let columnArr = [];
+
+    for (y = 0; y < columns; y++) {
+      // needs to be random num 1 - 100
+      columnArr.push(Math.floor(Math.random * 101));
+      // sort array
+      columnArr.sort();
+    }
+    final.push(columnArr);
+  }
+  return final;
 }
 
 module.exports = matrixGenerator;
